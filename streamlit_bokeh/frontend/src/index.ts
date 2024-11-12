5e55e6979f8c8ecbee119a2e0fb529788e4fbaa9
Ken McGrady <ken.mcgrady@snowflake.com>
2024-11-11 16:50:34 -0800

    Initial workings of Bokeh Chart

---
+++ streamlit_bokeh/frontend/src/index.ts
@@ -0,0 +1,154 @@
+import { Streamlit, RenderData, Theme } from "streamlit-component-lib"
+import { streamlitTheme } from "./streamlit-theme"
+
+declare global {
+  interface Window {
+    Bokeh: any
+  }
+}
+
+interface Dimensions {
+  width: number
+  height: number
+}
+
+// The div with id "stBokehChart" will always exist because html file contains it
+const chart = document.getElementById("stBokehChart") as HTMLDivElement
+
+const DEFAULT_WIDTH = 600 // px
+const DEFAULT_HEIGHT = 600 // px
+
+const getChartData = (() => {
+  let savedFigure: string | null = null
+  let savedChartData: object | null = null
+
+  return (figure: string) => {
+    if (figure !== savedFigure) {
+      savedFigure = figure
+      savedChartData = JSON.parse(figure)
+
+      return { data: savedChartData, hasChanged: true }
+    }
+
+    return { data: savedChartData, hasChanged: false }
+  }
+})()
+
+function getChartDimensions(plot: any, useContainerWidth: boolean): Dimensions {
+  const originalWidth: number = plot.attributes.width ?? DEFAULT_WIDTH
+  const originalHeight: number = plot.attributes.height ?? DEFAULT_HEIGHT
+
+  let width: number = originalWidth
+  let height: number = originalHeight
+
+  if (useContainerWidth) {
+    // Use the width without a scrollbar to ensure the width always
+    // looks good.
+    width = document.documentElement.clientWidth
+    height = (width / originalWidth) * originalHeight
+  }
+
+  return { width, height }
+}
+
+function removeAllChildNodes(element: Node): void {
+  while (element.lastChild) {
+    element.lastChild.remove()
+  }
+}
+
+async function updateChart(data: any, useContainerWidth: boolean = false) {
+  /**
+   * When you create a bokeh chart in your python script, you can specify
+   * the width: p = figure(title="simple line example", x_axis_label="x", y_axis_label="y", plot_width=200);
+   * In that case, the json object will contains an attribute called
+   * plot_width (or plot_heigth) inside the plot reference.
+   * If that values are missing, we can set that values to make the chart responsive.
+   */
+  const plot = data?.doc?.roots?.[0]
+
+  if (plot) {
+    const { width, height } = getChartDimensions(plot, useContainerWidth)
+
+    if (width > 0) {
+      plot.attributes.width = width
+    }
+    if (height > 0) {
+      plot.attributes.height = height
+    }
+  }
+
+  if (chart !== null) {
+    removeAllChildNodes(chart)
+    // embed_item is actually an async function call, so a race condition
+    // can occur if updateChart is called twice, leading to two Bokeh charts
+    // to be embedded at the same time.
+    await window.Bokeh.embed.embed_item(data, "stBokehChart")
+  }
+}
+
+interface ComponentData {
+  figure: string
+  use_container_width: boolean
+  bokeh_theme: string
+}
+
+let currentTheme: string | null = null
+let appTheme: string | null = null
+/**
+ * The component's render function. This will be called immediately after
+ * the component is initially loaded, and then again every time the
+ * component gets new data from Python.
+ */
+async function onRender(event: Event): Promise<void> {
+  const renderData: RenderData<ComponentData> = (
+    event as CustomEvent<RenderData<ComponentData>>
+  ).detail
+
+  const { data: chartData, hasChanged } = getChartData(renderData.args.figure)
+  const renderedAppTheme = JSON.stringify(renderData.theme)
+
+  let themeChanged = false
+  if (
+    renderData.args.bokeh_theme !== currentTheme ||
+    appTheme !== renderedAppTheme
+  ) {
+    currentTheme = renderData.args.bokeh_theme
+    appTheme = renderedAppTheme
+
+    const { use_theme } = window.Bokeh.require("core/properties")
+
+    if (currentTheme === "streamlit") {
+      use_theme(streamlitTheme(renderData.theme as Theme))
+      themeChanged = true
+    } else if (currentTheme in window.Bokeh.Themes) {
+      use_theme(window.Bokeh.Themes[currentTheme])
+    } else {
+      use_theme(streamlitTheme(renderData.theme as Theme))
+      themeChanged = true
+    }
+  }
+  if (hasChanged || themeChanged) {
+    await updateChart(chartData, renderData.args.use_container_width)
+  }
+
+  // RenderData.args is the JSON dictionary of arguments sent from the
+  // Python script.
+
+  // We tell Streamlit to update our frameHeight after each render event, in
+  // case it has changed. (This isn't strictly necessary for the example
+  // because our height stays fixed, but this is a low-cost function, so
+  // there's no harm in doing it redundantly.)
+  Streamlit.setFrameHeight()
+}
+
+// Attach our `onRender` handler to Streamlit's render event.
+Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender)
+
+// Tell Streamlit we're ready to start receiving data. We won't get our
+// first RENDER_EVENT until we call this function.
+Streamlit.setComponentReady()
+
+// Finally, tell Streamlit to update our initial height. We omit the
+// `height` parameter here to have it default to our scrollHeight.
+Streamlit.setFrameHeight()
---
+++ streamlit_bokeh/frontend/src/streamlit-theme.ts
@@ -0,0 +1,100 @@
+import { Theme } from "streamlit-component-lib"
+
+class BokehTheme {
+  readonly attrs: Record<string, Record<string, string | number>>
+
+  constructor(attrs: Record<string, Record<string, string | number>>) {
+    this.attrs = attrs
+  }
+
+  get(obj: any, attr: string): unknown | undefined {
+    let type = null
+    Object.keys(this.attrs).forEach((key) => {
+      if (obj instanceof window.Bokeh[key]) {
+        type = key
+      }
+    })
+
+    if (type === null) {
+      return undefined
+    }
+
+    try {
+      return this.attrs[type][attr]
+    } catch {
+      // Must not be set
+      return undefined
+    }
+  }
+}
+
+export function streamlitTheme(theme: Theme): BokehTheme {
+  // @ts-expect-error fadedText10 is not defined in the Theme type
+  const { backgroundColor, fadedText10, secondaryBackgroundColor, textColor } =
+    theme
+
+  return new BokehTheme({
+    Plot: {
+      background_fill_color: backgroundColor,
+      border_fill_color: backgroundColor,
+      outline_line_color: textColor,
+      outline_line_alpha: 0.25,
+    },
+    Grid: {
+      grid_line_color: fadedText10,
+      grid_line_alpha: 0.5,
+    },
+    Axis: {
+      major_tick_line_alpha: 0,
+      major_tick_line_color: textColor,
+
+      minor_tick_line_alpha: 0,
+      minor_tick_line_color: textColor,
+
+      axis_line_alpha: 0,
+      axis_line_color: textColor,
+
+      major_label_text_color: textColor,
+      major_label_text_font: '"Source Sans Pro", sans-serif',
+      major_label_text_font_size: "1em",
+
+      axis_label_standoff: 10,
+      axis_label_text_color: textColor,
+      axis_label_text_font: '"Source Sans Pro", sans-serif',
+      axis_label_text_font_size: "1em",
+      axis_label_text_font_style: "normal",
+    },
+    Legend: {
+      spacing: 8,
+      glyph_width: 15,
+
+      label_standoff: 8,
+      label_text_color: textColor,
+      label_text_font: '"Source Sans Pro", sans-serif',
+      label_text_font_size: "1.025em",
+
+      border_line_alpha: 0,
+      background_fill_alpha: 0.25,
+      background_fill_color: fadedText10,
+    },
+    BaseColorBar: {
+      title_text_color: textColor,
+      title_text_font: '"Source Sans Pro", sans-serif',
+      title_text_font_size: "1.025em",
+      title_text_font_style: "normal",
+
+      major_label_text_color: textColor,
+      major_label_text_font: '"Source Sans Pro", sans-serif',
+      major_label_text_font_size: "1.025em",
+
+      background_fill_color: secondaryBackgroundColor,
+      major_tick_line_alpha: 0,
+      bar_line_alpha: 0,
+    },
+    Title: {
+      text_color: textColor,
+      text_font: '"Source Sans Pro", sans-serif',
+      text_font_size: "1.15em",
+    },
+  })
+}
