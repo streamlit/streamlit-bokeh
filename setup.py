dde16dd33822f76cc661d87ad448ff8a88575db2
sfc-gh-kmcgrady <103003886+sfc-gh-kmcgrady@users.noreply.github.com>
2025-04-01 17:33:32 +0000

    Update Bokeh to 3.7.2 and release 3.7.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-bokeh",
-    version="3.7.0",
+    version="3.7.1",
     author="Snowflake Inc",
     author_email="hello@streamlit.io",
     description="Streamlit component that allows you to render Bokeh charts",
@@ -57,7 +57,7 @@ setuptools.setup(
         # If your component has other Python dependencies, list
         # them here.
         "streamlit>=1.26",
-        "bokeh==3.7.0",
+        "bokeh==3.7.2",
     ],
     extras_require={
         "devel": [
