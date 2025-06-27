8cba78711d3d027f2173ea1f1f6b7d8ff5124787
Bob Nisco <bob.nisco@snowflake.com>
2025-06-27 14:43:44 -0700

    Fix up declaration

--- streamlit_bokeh/__init__.py
+++ streamlit_bokeh/__init__.py
@@ -20,8 +20,10 @@ from typing import TYPE_CHECKING
 
 import bokeh
 import streamlit as st
-import streamlit.components.v1 as components
 from bokeh.embed import json_item
+
+if TYPE_CHECKING:
+    from bokeh.plotting.figure import Figure
 
 # Create a _RELEASE constant. We'll set this to False while we're developing
 # the component, and True when we're ready to package and distribute it.
@@ -30,38 +32,23 @@ from bokeh.embed import json_item
 _DEV = os.environ.get("DEV", False)
 _RELEASE = not _DEV
 
-# Declare a Streamlit component. `declare_component` returns a function
-# that is used to create instances of the component. We're naming this
-# function "_component_func", with an underscore prefix, because we don't want
-# to expose it directly to users. Instead, we will create a custom wrapper
-# function, below, that will serve as our component's public API.
-
-# It's worth noting that this call to `declare_component` is the
-# *only thing* you need to do to create the binding between Streamlit and
-# your component frontend. Everything else we do in this file is simply a
-# best practice.
+component_name = "streamlit_bokeh"
 
 if not _RELEASE:
-    _component_func = components.declare_component(
-        # We give the component a simple, descriptive name ("streamlit_bokeh"
-        # does not fit this bill, so please choose something better for your
-        # own component :)
-        "streamlit_bokeh",
-        # Pass `url` here to tell Streamlit that the component will be served
-        # by the local dev server that you run via `npm run start`.
-        # (This is useful while your component is in development.)
-        url="http://localhost:3001",
+    _component_func = st.components.v2.component(
+        component_name,
+        js=Path(__file__).parent / "frontend/build/index.mjs",
+        html="""<div class="stBokehContainer"></div>""",
     )
 else:
-    # When we're distributing a production version of the component, we'll
-    # replace the `url` param with `path`, and point it to the component's
-    # build directory:
+    # TODO: Production
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, "frontend/build")
-    _component_func = components.declare_component("streamlit_bokeh", path=build_dir)
+    _component_func = st.components.v2.component(
+        component_name,
+        path=build_dir,
+    )
 
-if TYPE_CHECKING:
-    from bokeh.plotting.figure import Figure
 
 __version__ = importlib.metadata.version("streamlit_bokeh")
 REQUIRED_BOKEH_VERSION = "3.7.3"
@@ -115,22 +102,15 @@ def streamlit_bokeh(
             f"{REQUIRED_BOKEH_VERSION}` to install the correct version."
         )
 
-    # Call through to our private component function. Arguments we pass here
-    # will be sent to the frontend, where they'll be available in an "args"
-    # dictionary.
-    # _component_func(
-    #     figure=json.dumps(json_item(figure)),
-    #     use_container_width=use_container_width,
-    #     bokeh_theme=theme,
-    #     key=key,
-    # )
-    out = st.components.v2.component(
-        "streamlit_bokeh",
-        js=Path(__file__).parent / "frontend/build/assets/index-CbIqqD5M.js",
+    # Call through to our private component function.
+    out = _component_func(
         key=key,
         data={
             "figure": json.dumps(json_item(figure)),
             "bokeh_theme": theme,
+            "use_container_width": use_container_width,
+            "key": key,
         },
+        isolate_styles=False,
     )
     return out
--- streamlit_bokeh/example.py
+++ streamlit_bokeh/example.py
@@ -12,100 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
-from pathlib import Path
-from typing import TYPE_CHECKING
 
-import bokeh
 import streamlit as st
-from bokeh.embed import json_item
 from bokeh.plotting import figure
 
-if TYPE_CHECKING:
-    from bokeh.plotting.figure import Figure
-
-
-# __version__ = importlib.metadata.version("streamlit_bokeh")
-__version__ = "3.7.3"
-REQUIRED_BOKEH_VERSION = "3.7.3"
-
-
-def streamlit_bokeh(
-    figure: "Figure",
-    use_container_width: bool = True,
-    theme: str = "streamlit",
-    key: str | None = None,
-) -> None:
-    """Create a new instance of "streamlit_bokeh".
-
-    Parameters
-    ----------
-    figure: bokeh.plotting.figure.Figure
-        A Bokeh figure to plot.
-    use_container_width : bool
-        Whether to override the figure's native width with the width of
-        the parent container. If ``use_container_width`` is ``False``,
-        Streamlit sets the width of the chart to fit its contents
-        according to the plotting library, up to the width of the parent
-        container. If ``use_container_width`` is ``True`` (default), Streamlit
-        sets the width of the figure to match the width of the parent container.
-    key: str or None
-        An optional key that uniquely identifies this component. If this is
-        None, and the component's arguments are changed, the component will
-        be re-mounted in the Streamlit frontend and lose its current state.
-
-    Example
-    -------
-    >>> from streamlit_bokeh import streamlit_bokeh
-    >>> from bokeh.plotting import figure
-    >>>
-    >>> x = [1, 2, 3, 4, 5]
-    >>> y = [6, 7, 2, 4, 5]
-    >>>
-    >>> p = figure(title="simple line example", x_axis_label="x", y_axis_label="y")
-    >>> p.line(x, y, legend_label="Trend", line_width=2)
-    >>>
-    >>> streamlit_bokeh(p, use_container_width=True)
-
-    """
-
-    if bokeh.__version__ != REQUIRED_BOKEH_VERSION:
-        # TODO(ken): Update Error message
-        raise Exception(
-            f"Streamlit only supports Bokeh version {REQUIRED_BOKEH_VERSION}, "
-            f"but you have version {bokeh.__version__} installed. Please "
-            f"run `pip install --force-reinstall --no-deps bokeh=="
-            f"{REQUIRED_BOKEH_VERSION}` to install the correct version."
-        )
-
-    # Call through to our private component function. Arguments we pass here
-    # will be sent to the frontend, where they'll be available in an "args"
-    # dictionary.
-    # _component_func(
-    #     figure=json.dumps(json_item(figure)),
-    #     use_container_width=use_container_width,
-    #     bokeh_theme=theme,
-    #     key=key,
-    # )
-
-    out = st.components.v2.component(
-        "streamlit_bokeh",
-        js=Path(__file__).parent / "frontend/build/index.mjs",
-        html="""<div class="stBokehContainer"></div>""",
-        # html=Path(__file__).parent / "frontend/build/index.html",
-        key=key,
-        data={
-            "figure": json.dumps(json_item(figure)),
-            "bokeh_theme": theme,
-            "use_container_width": use_container_width,
-            "key": key,
-        },
-        # Given the Bokeh.js API, which relies on `document.getElementById`, we
-        # must turn this off so that the DOM element can be found.
-        isolate_styles=False,
-    )
-    return out
-
+from streamlit_bokeh import streamlit_bokeh
 
 st.title("Streamlit Bokeh Example")
 
