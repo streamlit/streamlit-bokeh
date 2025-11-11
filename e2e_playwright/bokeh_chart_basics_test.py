3a8502051b5ed4c85153727e5b1c0c489dfef66d
Bob Nisco <bob.nisco@snowflake.com>
2025-11-10 20:32:41 -0800

    Test in both v1 and v2 modes

--- e2e_playwright/bokeh_chart_basics_test.py
+++ e2e_playwright/bokeh_chart_basics_test.py
@@ -20,7 +20,7 @@ from playwright.sync_api import Page, expect
 
 @pytest.mark.parametrize("chart", CHART_TYPES)
 def test_bokeh_chart(
-    themed_app: Page, assert_snapshot: ImageCompareFunction, chart: str
+    themed_app: Page, assert_snapshot: ImageCompareFunction, chart: str, is_v2: bool
 ):
     """Test that st.bokeh_chart renders correctly."""
     themed_app.get_by_test_id("stSelectbox").locator("input").click()
@@ -30,13 +30,25 @@ def test_bokeh_chart(
     selection_dropdown.get_by_text(chart).click()
 
     wait_for_app_run(themed_app)
-    instances = themed_app.locator("[data-testid=stBidiComponentRegular]")
-    EXPECTED_INSTANCE_COUNT = 2
-    expect(instances).to_have_count(EXPECTED_INSTANCE_COUNT)
-
-    for idx in range(EXPECTED_INSTANCE_COUNT):
-        label = "use-container-width" if idx == 1 else "standard-width"
-        instance = instances.nth(idx)
-        canvas = instance.locator("div.bk-Canvas")
-        expect(canvas).to_be_visible()
-        assert_snapshot(instance, name=f"bokeh_chart-{chart}-{label}")
+    if is_v2:
+        # Custom Component v2 renders inline without iframes
+        instances = themed_app.locator("[data-testid=stBidiComponentRegular]")
+        EXPECTED_INSTANCE_COUNT = 2
+        expect(instances).to_have_count(EXPECTED_INSTANCE_COUNT)
+        for idx in range(EXPECTED_INSTANCE_COUNT):
+            label = "use-container-width" if idx == 1 else "standard-width"
+            instance = instances.nth(idx)
+            canvas = instance.locator("div.bk-Canvas")
+            expect(canvas).to_be_visible()
+            assert_snapshot(instance, name=f"bokeh_chart-{chart}-{label}")
+    else:
+        # Custom Component v1 renders inside iframes
+        iframes = themed_app.locator("iframe")
+        IFRAME_COUNT = 2
+        expect(iframes).to_have_count(IFRAME_COUNT)
+        for idx in range(IFRAME_COUNT):
+            label = "use-container-width" if idx == 1 else "standard-width"
+            iframe = iframes.nth(idx)
+            canvas = iframe.content_frame.locator("div.bk-Canvas")
+            expect(canvas).to_be_visible()
+            assert_snapshot(iframe, name=f"bokeh_chart-{chart}-{label}")
