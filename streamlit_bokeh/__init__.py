1e3bc1a2ab61a89789584c630c9b7a11ad0e8f54
sfc-gh-kmcgrady <103003886+sfc-gh-kmcgrady@users.noreply.github.com>
2025-11-11 17:34:16 +0000

    Update Bokeh to 3.8.1 and release 3.8.1

--- streamlit_bokeh/__init__.py
+++ streamlit_bokeh/__init__.py
@@ -61,7 +61,7 @@ if TYPE_CHECKING:
     from bokeh.plotting.figure import Figure
 
 __version__ = importlib.metadata.version("streamlit_bokeh")
-REQUIRED_BOKEH_VERSION = "3.8.0"
+REQUIRED_BOKEH_VERSION = "3.8.1"
 
 
 def streamlit_bokeh(
